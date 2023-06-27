07a2274d84525b1d107912684c5d1a152aaa6a62
BeastBane24 <112552319+BeastBane24@users.noreply.github.com>
2023-06-27 14:31:43 +0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,50 @@
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+      apiKey: "AIzaSyC0JjcV45JJ023XkYpwZZMxmMSCNhTP1N4",
+      authDomain: "kwitter2-e9725.firebaseapp.com",
+      databaseURL: "https://kwitter2-e9725-default-rtdb.firebaseio.com",
+      projectId: "kwitter2-e9725",
+      storageBucket: "kwitter2-e9725.appspot.com",
+      messagingSenderId: "127653710229",
+      appId: "1:127653710229:web:90221f2f01365e174b8e9c"
+    };
+
+firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom() {
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+      window.location = "kwitter_page.html";
+}
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  console.log("Room Name - " + Room_names);
+                  row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+                  document.getElementById("output").innerHTML += row;
+            });
+      });
+}
+getData();
+
+function redirectToRoomName(name) {
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
